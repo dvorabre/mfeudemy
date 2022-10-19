8bcef2031ff029d934568cb863600fbda99dc993
dvorabre <110549048+dvorabre@users.noreply.github.com>
2022-10-19 13:11:50 +0300

    add navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,27 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({pathname: nextPathname}) => {
+        // listen get a location object that contain pathname property hith the path
+        const { pathname } = history.location;
+        if (pathname !== nextPathname){
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    // move to the module when navigate happened
+    history.listen(onParentNavigate);
+
+  }, []);
 
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  // listen to history and call callback.
+  if(onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    // move the function to the call, that the parent will can call children
+    onParentNavigate({ pathname: nextPathname}){
+
+      const { pathname } = history.location;
+      if (pathname !== nextPathname){
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 // If we are in development and in isolation,
@@ -13,7 +31,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
